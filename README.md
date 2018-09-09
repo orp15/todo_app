79c6a214cb8b3acd6bee4ef6855bec304175f01f
oscar padilla <orp_15@unitec.edu>
2018-09-09 10:21:42 -0600

    primer comentario

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-todo_app
+# Todo app
+
+Esta es una prueba de la sintasix para archivos README.md
+
+## Nombre de la aplicacion
+
+__TODO APP__
+
+![Nombre de la aplicacion](/static/description/icon.png)
---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Todo App',
+    'description': ''' Esta es una aplicacion para notas ''',
+    'author': 'Oscar Padilla',
+    'application': True,
+    'depends': ['base'],
+}
